28f45b6190b6fa4048716a4c5f2bebf5dbb561dc
KaiNong <na850109th@gmail.com>
2020-05-30 15:35:33 +0800

    newsort4

--- MySorter.java
+++ MySorter.java
@@ -3,7 +3,7 @@ public class MySorter {
 	public double[] sort(double[] d) {
 		if (d.length == 0) {
 			double[] d2 = { 0.0 };
-			return gggggggggg;
+			return wwwwwwwwwwwwwwwwwwwwwww;
 		} else {
 			for (int i = 0; i < d.length - 1; i++) {
 				for (int j = 0; j < d.length - i - 1; j++) {
